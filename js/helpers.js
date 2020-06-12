98eabf0202d38258d80079018ab2ed3d6b70857d
brookefzy <yuanzf@mit.edu>
2020-06-12 13:53:20 -0400

    isp maps blue

--- js/helpers.js
+++ js/helpers.js
@@ -127,3 +127,61 @@ function convertArrayOfObjectsToCSV(args) {
 
   return result;
 }
+
+function getUniqueFeatures(array, comparatorProperty) {
+  var existingFeatureKeys = {};
+  // Because features come from tiled vector data, feature geometries may be split
+  // or duplicated across tile boundaries and, as a result, features may appear
+  // multiple times in query results.
+  var uniqueFeatures = array.filter(function (el) {
+    if (existingFeatureKeys[el.properties[comparatorProperty]]) {
+      return false;
+    } else {
+      existingFeatureKeys[el.properties[comparatorProperty]] = true;
+      return true;
+    }
+  });
+
+  return uniqueFeatures;
+}
+
+var createGeoJSONCircle = function (center, radiusInKm, points) {
+  if (!points) points = 64;
+
+  var coords = {
+    latitude: center[1],
+    longitude: center[0],
+  };
+
+  var km = radiusInKm;
+
+  var ret = [];
+  var distanceX = km / (111.32 * Math.cos((coords.latitude * Math.PI) / 180));
+  var distanceY = km / 110.574;
+
+  var theta, x, y;
+  for (var i = 0; i < points; i++) {
+    theta = (i / points) * (2 * Math.PI);
+    x = distanceX * Math.cos(theta);
+    y = distanceY * Math.sin(theta);
+
+    ret.push([coords.longitude + x, coords.latitude + y]);
+  }
+  ret.push(ret[0]);
+
+  return {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            coordinates: [ret],
+          },
+        },
+      ],
+    },
+  };
+};
